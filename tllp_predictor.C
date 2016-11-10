943ed44046cbbb9f4629f2a3f4db2111fbae3386
bdaemon <amit.borase@yahoo.in>
2016-11-09 23:21:52 -0800

    perf fine tuning of 2llpredictor

--- tllp_predictor.C
+++ tllp_predictor.C
@@ -1,12 +1,15 @@
 // A very stupid predictor.  It will always predict not taken.
 #include <stdio.h>
+#include <assert.h>
 #include "tllp_predictor.h"
 
 /* Budget specific globals set during init. */
-unsigned int tllp_size;
+unsigned int tllp_lht_size;
+unsigned int tllp_bpt_size;
 unsigned int tllp_bpt_mid;
-unsigned int tllp_lht_max;
-unsigned int  tllp_bpt_max;
+unsigned int tllp_bpt_max;
+unsigned int tllp_bpt_idx_max;
+unsigned int tllp_lht_idx_max;
 unsigned int *tllp_lht;
 unsigned int *tllp_bpt;
 tllp_table tllp;
@@ -15,57 +18,69 @@ static void tllp_set_budget_constants(budget_size budget)
 {
 	switch (budget) {
 		case BUDGET_8K:
-			tllp_size 	 = TLLP_SIZE_8K;
-			tllp_bpt_mid = TLLP_BPT_MID_8K;
-			tllp_lht_max = TLLP_LHT_MAX_8K;
-			tllp_bpt_max = TLLP_BPT_MAX_8K;
-			tllp_lht     = tllp.lht.b8k;
-			tllp_bpt     = tllp.bpt.b8k;
+			tllp_lht_size		= TLLP_LHT_SIZE_8K;
+			tllp_bpt_size		= TLLP_BPT_SIZE_8K;
+			tllp_bpt_mid		= TLLP_BPT_MID_8K;
+			tllp_bpt_max		= TLLP_BPT_MAX_8K;
+			tllp_bpt_idx_max	= TLLP_BPT_IDX_MAX_8K;
+			tllp_lht_idx_max	= TLLP_LHT_IDX_MAX_8K;
+			tllp_lht			= tllp.lht.b8k;
+			tllp_bpt			= tllp.bpt.b8k;
 			break;
 
 		case BUDGET_16K:
-			tllp_size 	 = TLLP_SIZE_16K;
-			tllp_bpt_mid = TLLP_BPT_MID_16K;
-			tllp_lht_max = TLLP_LHT_MAX_16K;
-			tllp_bpt_max = TLLP_BPT_MAX_16K;
-			tllp_lht     = tllp.lht.b16k;
-			tllp_bpt     = tllp.bpt.b16k;
+			tllp_lht_size		= TLLP_LHT_SIZE_16K;
+			tllp_bpt_size		= TLLP_BPT_SIZE_16K;
+			tllp_bpt_mid		= TLLP_BPT_MID_16K;
+			tllp_bpt_max		= TLLP_BPT_MAX_16K;
+			tllp_bpt_idx_max	= TLLP_BPT_IDX_MAX_16K;
+			tllp_lht_idx_max	= TLLP_LHT_IDX_MAX_16K;
+			tllp_lht			= tllp.lht.b16k;
+			tllp_bpt			= tllp.bpt.b16k;
 			break;
 
 		case BUDGET_32K:
-			tllp_size 	 = TLLP_SIZE_32K;
-			tllp_bpt_mid = TLLP_BPT_MID_32K;
-			tllp_lht_max = TLLP_LHT_MAX_32K;
-			tllp_bpt_max = TLLP_BPT_MAX_32K;
-			tllp_lht     = tllp.lht.b32k;
-			tllp_bpt     = tllp.bpt.b32k;
+			tllp_lht_size		= TLLP_LHT_SIZE_32K;
+			tllp_bpt_size		= TLLP_BPT_SIZE_32K;
+			tllp_bpt_mid		= TLLP_BPT_MID_32K;
+			tllp_bpt_max		= TLLP_BPT_MAX_32K;
+			tllp_bpt_idx_max	= TLLP_BPT_IDX_MAX_32K;
+			tllp_lht_idx_max	= TLLP_LHT_IDX_MAX_32K;
+			tllp_lht			= tllp.lht.b32k;
+			tllp_bpt			= tllp.bpt.b32k;
 			break;
 
 		case BUDGET_64K:
-			tllp_size 	 = TLLP_SIZE_32K;
-			tllp_bpt_mid = TLLP_BPT_MID_32K;
-			tllp_lht_max = TLLP_LHT_MAX_32K;
-			tllp_bpt_max = TLLP_BPT_MAX_32K;
-			tllp_lht     = tllp.lht.b32k;
-			tllp_bpt     = tllp.bpt.b32k;
+			tllp_lht_size		= TLLP_LHT_SIZE_64K;
+			tllp_bpt_size		= TLLP_BPT_SIZE_64K;
+			tllp_bpt_mid		= TLLP_BPT_MID_64K;
+			tllp_bpt_max		= TLLP_BPT_MAX_64K;
+			tllp_bpt_idx_max	= TLLP_BPT_IDX_MAX_64K;
+			tllp_lht_idx_max	= TLLP_LHT_IDX_MAX_64K;
+			tllp_lht			= tllp.lht.b64k;
+			tllp_bpt			= tllp.bpt.b64k;
 			break;
 
 		case BUDGET_128K:
-			tllp_size 	 = TLLP_SIZE_128K;
-			tllp_bpt_mid = TLLP_BPT_MID_128K;
-			tllp_lht_max = TLLP_LHT_MAX_128K;
-			tllp_bpt_max = TLLP_BPT_MAX_128K;
-			tllp_lht     = tllp.lht.b128k;
-			tllp_bpt     = tllp.bpt.b128k;
+			tllp_lht_size		= TLLP_LHT_SIZE_128K;
+			tllp_bpt_size		= TLLP_BPT_SIZE_128K;
+			tllp_bpt_mid		= TLLP_BPT_MID_128K;
+			tllp_bpt_max		= TLLP_BPT_MAX_128K;
+			tllp_bpt_idx_max	= TLLP_BPT_IDX_MAX_128K;
+			tllp_lht_idx_max	= TLLP_LHT_IDX_MAX_128K;
+			tllp_lht			= tllp.lht.b128k;
+			tllp_bpt			= tllp.bpt.b128k;
 			break;
 
 		case BUDGET_1M:
-			tllp_size 	 = TLLP_SIZE_1M;
-			tllp_bpt_mid = TLLP_BPT_MID_1M;
-			tllp_lht_max = TLLP_LHT_MAX_1M;
-			tllp_bpt_max = TLLP_BPT_MAX_1M;
-			tllp_lht     = tllp.lht.b1m;
-			tllp_bpt     = tllp.bpt.b1m;
+			tllp_lht_size		= TLLP_LHT_SIZE_1M;
+			tllp_bpt_size		= TLLP_BPT_SIZE_1M;
+			tllp_bpt_mid		= TLLP_BPT_MID_1M;
+			tllp_bpt_max		= TLLP_BPT_MAX_1M;
+			tllp_bpt_idx_max	= TLLP_BPT_IDX_MAX_1M;
+			tllp_lht_idx_max	= TLLP_LHT_IDX_MAX_1M;
+			tllp_lht			= tllp.lht.b1m;
+			tllp_bpt			= tllp.bpt.b1m;
 			break;
 
 		default:
@@ -78,18 +93,19 @@ void init_tllp_predictor (budget_size budget)
 {
 	tllp_set_budget_constants(budget);	
 
-	memset(tllp_lht, 0, sizeof(unsigned int) * tllp_size);
-	for (int i = 0; i < tllp_size; i++) {
+	memset(tllp_lht, 0, sizeof(unsigned int) * tllp_lht_size);
+	for (int i = 0; i < tllp_bpt_size; i++) {
 		tllp_bpt[i] = tllp_bpt_max;
 	}
 }
 
 bool make_tllp_prediction (unsigned int pc)
 {
-	unsigned int lsb_addr = (pc & tllp_lht_max);
+	unsigned int lsb_addr = (pc & tllp_lht_idx_max);
 	unsigned int bpt_idx = tllp_lht[lsb_addr];
 
 	if (tllp_bpt[bpt_idx] > tllp_bpt_mid) {
+		assert(tllp_bpt[bpt_idx] <= tllp_bpt_max);
 		return true;
 	} else {
 		return false;
@@ -98,7 +114,7 @@ bool make_tllp_prediction (unsigned int pc)
 
 void train_tllp_predictor (unsigned int pc, bool outcome)
 {
-	unsigned int lsb_addr = (pc & tllp_lht_max);
+	unsigned int lsb_addr = (pc & tllp_lht_idx_max);
 	unsigned int bpt_idx = tllp_lht[lsb_addr];
 
 	if (outcome) {
@@ -109,11 +125,12 @@ void train_tllp_predictor (unsigned int pc, bool outcome)
 		bpt_idx = ((bpt_idx << 1) | 0x01);
 	} else {
 		if (tllp_bpt[bpt_idx] > 0) {
+			assert(tllp_bpt[bpt_idx] <= tllp_bpt_max);
 			tllp_bpt[bpt_idx]--;
 		}
 
 		bpt_idx = ((bpt_idx << 1) | 0x00);
 	}
 
-	tllp_lht[lsb_addr] = (bpt_idx & tllp_lht_max);
+	tllp_lht[lsb_addr] = (bpt_idx & tllp_bpt_idx_max);
 }
--- tllp_predictor.h
+++ tllp_predictor.h
@@ -19,57 +19,69 @@
 */
 
 /* For budget 8K */
-#define TLLP_SIZE_8K 512
-#define TLLP_BPT_MID_8K 0x0000003f
-#define TLLP_LHT_MAX_8K 0x000001ff
-#define TLLP_BPT_MAX_8K 0x0000007f
+#define TLLP_LHT_SIZE_8K 2048
+#define TLLP_BPT_SIZE_8K 16
+#define TLLP_BPT_MID_8K 0x00000003
+#define TLLP_BPT_MAX_8K 0x00000007
+#define TLLP_BPT_IDX_MAX_8K 0x0000000f
+#define TLLP_LHT_IDX_MAX_8K 0x000007ff
 
 /* For budget 16K */
-#define TLLP_SIZE_16K 1024
-#define TLLP_BPT_MID_16K 0x0000001f
-#define TLLP_LHT_MAX_16K 0x000003ff
-#define TLLP_BPT_MAX_16K 0x0000003f
+#define TLLP_LHT_SIZE_16K 4096
+#define TLLP_BPT_SIZE_16K 16
+#define TLLP_BPT_MID_16K 0x00000003
+#define TLLP_BPT_MAX_16K 0x00000007
+#define TLLP_BPT_IDX_MAX_16K 0x0000000f
+#define TLLP_LHT_IDX_MAX_16K 0x00000fff
 
 /* For budget 32K */
-#define TLLP_SIZE_32K 2048
-#define TLLP_BPT_MID_32K 0x0000000f
-#define TLLP_LHT_MAX_32K 0x000007ff
-#define TLLP_BPT_MAX_32K 0x0000001f
+#define TLLP_LHT_SIZE_32K 8192
+#define TLLP_BPT_SIZE_32K 16
+#define TLLP_BPT_MID_32K 0x00000003
+#define TLLP_BPT_MAX_32K 0x00000007
+#define TLLP_BPT_IDX_MAX_32K 0x0000000f
+#define TLLP_LHT_IDX_MAX_32K 0x000001fff
 
 /* for budget 64K */
-#define TLLP_SIZE_64K 4096
-#define TLLP_BPT_MID_64K 0x00000007
-#define TLLP_LHT_MAX_64K 0x00000fff
-#define TLLP_BPT_MAX_64K 0x0000000f
+#define TLLP_LHT_SIZE_64K 16384
+#define TLLP_BPT_SIZE_64K 16
+#define TLLP_BPT_MID_64K 0x00000003
+#define TLLP_BPT_MAX_64K 0x00000007
+#define TLLP_BPT_IDX_MAX_64K 0x0000000f
+#define TLLP_LHT_IDX_MAX_64K 0x00003fff
 
 /* For budget 128K */
-#define TLLP_SIZE_128K 8192
+#define TLLP_LHT_SIZE_128K 32768
+#define TLLP_BPT_SIZE_128K 16
 #define TLLP_BPT_MID_128K 0x00000003
-#define TLLP_LHT_MAX_128K 0x00001fff
 #define TLLP_BPT_MAX_128K 0x00000007
+#define TLLP_BPT_IDX_MAX_128K 0x0000000f
+#define TLLP_LHT_IDX_MAX_128K 0x00007fff
 
 /* For budget 1M */
-#define TLLP_SIZE_1M 32768
-#define TLLP_BPT_MID_1M 0x00003fff
-#define TLLP_LHT_MAX_1M 0x00007fff
-#define TLLP_BPT_MAX_1M 0x00007fff
+#define TLLP_LHT_SIZE_1M 262144
+#define TLLP_BPT_SIZE_1M 16
+#define TLLP_BPT_MID_1M 0x00000003
+#define TLLP_BPT_MAX_1M 0x00000007
+#define TLLP_BPT_IDX_MAX_1M 0x0000000f
+#define TLLP_LHT_IDX_MAX_1M 0x0003ffff
 
 typedef struct {
 	union {
-		unsigned int b8k[TLLP_SIZE_8K];
-		unsigned int b16k[TLLP_SIZE_16K];
-		unsigned int b32k[TLLP_SIZE_32K];
-		unsigned int b64k[TLLP_SIZE_64K];
-		unsigned int b128k[TLLP_SIZE_128K];
-		unsigned int b1m[TLLP_SIZE_1M];
+		unsigned int b8k[TLLP_LHT_SIZE_8K];
+		unsigned int b16k[TLLP_LHT_SIZE_16K];
+		unsigned int b32k[TLLP_LHT_SIZE_32K];
+		unsigned int b64k[TLLP_LHT_SIZE_64K];
+		unsigned int b128k[TLLP_LHT_SIZE_128K];
+		unsigned int b1m[TLLP_LHT_SIZE_1M];
 	} lht;
 	union {
-		unsigned int b8k[TLLP_SIZE_8K];
-		unsigned int b16k[TLLP_SIZE_16K];
-		unsigned int b32k[TLLP_SIZE_32K];
-		unsigned int b64k[TLLP_SIZE_64K];
-		unsigned int b128k[TLLP_SIZE_128K];
-		unsigned int b1m[TLLP_SIZE_1M];
+		unsigned int b8k[TLLP_BPT_SIZE_8K];
+		unsigned int b16k[TLLP_BPT_SIZE_16K];
+		unsigned int b32k[TLLP_BPT_SIZE_32K];
+		unsigned int b64k[TLLP_BPT_SIZE_64K];
+		unsigned int b128k[TLLP_BPT_SIZE_128K];
+		unsigned int b1m[TLLP_BPT_SIZE_1M];
 	} bpt;
 } tllp_table;
 
